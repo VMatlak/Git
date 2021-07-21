69bcf065df5392d9e9ffd5b28c22263456215ec4
VMatlak <matlack.viktor@gmail.com>
2021-07-21 18:33:07 +0300

    commit_2

--- Git/Git.cpp
+++ Git/Git.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Commit_1\n";
+    std::cout << "Commit_2\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
